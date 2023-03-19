80a5d802f16832fa4d3c0f24e23cc59b2c67fb2b
Temitope Bankole <bankson03@gmail.com>
2023-03-19 19:22:24 +0100

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+          <input type="text" v-model="username" placeholder="username" />
+        </div>
+        <div>
+          <button @click="fetchRepo" :disabled="!username">Fetch Repository</button>
+        </div>
+
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+    setup() {
+        const username = ref("");
+
+        return { username }
+    }
+};
+
+</script>
